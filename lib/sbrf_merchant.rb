68f611d0ecb38c587fa53a753baa7ee996715046
Eugene Kozlov <kozlovea8@gmail.com>
2019-10-23 16:17:29 +0300

    Drop #configure in SbrfMerchant

--- lib/sbrf_merchant.rb
+++ lib/sbrf_merchant.rb
@@ -1,24 +1,8 @@
+# frozen_string_literal: true
+
 require 'sbrf_merchant/api/client'
 
 module SbrfMerchant
-  class << self
-    attr_accessor :configuration
-
-    def configure
-      self.configuration ||= Configuration.new
-      yield(configuration)
-    end
-  end
-
-  class Configuration
-    attr_accessor :host, :user_name, :password
-
-    def initialize(host: nil, user_name: nil, password: nil)
-      @host = host
-      @user_name = user_name
-      @password = password
-    end
-  end
 end
 
 Sberbank = SbrfMerchant
